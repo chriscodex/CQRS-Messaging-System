2d79e598e39e6a7a480d36319793bfd5e2e36afe
Christian <3003html@gmail.com>
2022-08-11 01:55:55 -0500

    SetRepository function created

--- repository/repository.go
+++ repository/repository.go
@@ -6,7 +6,14 @@ import (
 	"github.com/ChrisCodeX/Event-Architecture-CQRS-Go/models"
 )
 
-type repository interface {
+type Repository interface {
 	Close()
 	InsertFeed(ctx context.Context, feed *models.Feed) error
+	ListFeeds(ctx context.Context) ([]*models.Feed, error)
+}
+
+var repository Repository
+
+func SetRepository(r Repository) {
+	repository = r
 }
