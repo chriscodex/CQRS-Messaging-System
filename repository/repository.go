ceaf9a5fb662c0e9417f52b595c10aa313be259a
Christian <3003html@gmail.com>
2022-08-11 01:53:28 -0500

    repository interface created

--- repository/repository.go
+++ repository/repository.go
@@ -1,1 +1,12 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/ChrisCodeX/Event-Architecture-CQRS-Go/models"
+)
+
+type repository interface {
+	Close()
+	InsertFeed(ctx context.Context, feed *models.Feed) error
+}
