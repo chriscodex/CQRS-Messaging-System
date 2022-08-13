f60c2a6928e5768387957f85bb95e440a6d49e68
Christian <3003html@gmail.com>
2022-08-12 20:14:07 -0500

    CreatedFeedMessage struct created for transfer through NATS

--- events/messages.go
+++ events/messages.go
@@ -1,6 +1,16 @@
 package events
 
+import "time"
+
 // Dependency inversion interface
 type Message interface {
 	Type() string
+}
+
+// Struct that will be transmitted through the NATS
+type CreatedFeedMessage struct {
+	Id          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
 }
