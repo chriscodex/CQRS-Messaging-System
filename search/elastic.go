3cfbf46efbeb11057e25fba8914df3cead8f0c31
Christian <3003html@gmail.com>
2022-08-14 18:27:49 -0500

    Constructor created for ElasticSearchRepository

--- search/elastic.go
+++ search/elastic.go
@@ -1,1 +1,20 @@
 package search
+
+import (
+	elastic "github.com/elastic/go-elasticsearch/v7"
+)
+
+type ElasticSearchRepository struct {
+	client *elastic.Client
+}
+
+// Constructor
+func NewElastic(url string) (*ElasticSearchRepository, error) {
+	client, err := elastic.NewClient(elastic.Config{
+		Addresses: []string{url},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &ElasticSearchRepository{client: client}, nil
+}
