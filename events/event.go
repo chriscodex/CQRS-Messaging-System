d9e422f8548c54df9b4f3786ead89bec7df15904
Christian <3003html@gmail.com>
2022-08-13 20:32:08 -0500

    Methods of abstract implementation eventStore interface created

--- events/event.go
+++ events/event.go
@@ -1,1 +1,37 @@
 package events
+
+import (
+	"context"
+
+	"github.com/ChrisCodeX/Event-Architecture-CQRS-Go/models"
+)
+
+/* Abstract implementation of NATS */
+type EventStore interface {
+	Close()
+	// Publish new feed event
+	PublishCreatedFeed(ctx context.Context, feed *models.Feed) error
+	// Subscribe to feed event
+	SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error)
+	// Callback when new feed is created
+	OnCreateFeed(f func(CreatedFeedMessage)) error
+}
+
+var eventStore EventStore
+
+/*Methods*/
+func Close() {
+	eventStore.Close()
+}
+
+func PublishCreatedFeed(ctx context.Context, feed *models.Feed) error {
+	return eventStore.PublishCreatedFeed(ctx, feed)
+}
+
+func SubscribeCreatedFeed(ctx context.Context) (<-chan CreatedFeedMessage, error) {
+	return eventStore.SubscribeCreatedFeed(ctx)
+}
+
+func OnCreateFeed(f func(CreatedFeedMessage)) error {
+	return OnCreateFeed(f)
+}
