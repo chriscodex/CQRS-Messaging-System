c90119d6ed57db414866c5caf89715d789f2babe
Christian <3003html@gmail.com>
2022-08-15 02:08:30 -0500

    createFeedHandler function created

--- feedservice/handler.go
+++ feedservice/handler.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type createFeedRequest struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
+}
+
+func createFeedHandler(w http.ResponseWriter, r *http.Request) {
+	// Decode the request from server
+	var req createFeedRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
 }
