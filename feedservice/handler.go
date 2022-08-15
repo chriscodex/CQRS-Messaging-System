00b6e117740d202eb0d8fa18c7dfaeb54c58071f
Christian <3003html@gmail.com>
2022-08-15 02:09:01 -0500

    generate id added

--- feedservice/handler.go
+++ feedservice/handler.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/segmentio/ksuid"
 )
 
 type createFeedRequest struct {
@@ -15,6 +18,16 @@ func createFeedHandler(w http.ResponseWriter, r *http.Request) {
 	var req createFeedRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Time of feed created
+	createdAt := time.Now().UTC()
+
+	// Generate id
+	id, err := ksuid.NewRandom()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 }
