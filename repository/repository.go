4558951948a1e2b60b5044454698d784fc68dce1
Christian <gonzalo.espinozacadillo@gmail.com>
2022-09-26 15:59:39 -0500

    repository name changed

--- repository/repository.go
+++ repository/repository.go
@@ -3,7 +3,7 @@ package repository
 import (
 	"context"
 
-	"github.com/ChrisCodeX/Event-Architecture-CQRS-Go/models"
+	"github.com/ChrisCodeX/CQRS-Messaging-System/models"
 )
 
 type Repository interface {
